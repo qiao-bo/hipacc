2487d8502956ad42acb3b51dc6e98c27aca75c78
Julian Brandl <julian.brandl@fau.de>
2020-07-02 15:20:40 +0200

    Add sample for DFT and DCT usage

---
+++ apps/7_FFT/DFT_DCT/src/main.cpp
@@ -0,0 +1,117 @@
+//
+// Copyright (c) 2012, University of Erlangen-Nuremberg
+// Copyright (c) 2012, Siemens AG
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include "hipacc.hpp"
+
+#include <hipacc_helper.hpp>
+#include <iostream>
+
+#define WIDTH 4032
+#define HEIGHT 3024
+#define IMAGE "../../common/img/fuerte_ship.jpg"
+
+#define TYPE float
+#define TYPE2 double
+
+using namespace hipacc;
+using namespace hipacc::math;
+
+/*************************************************************************
+ * Main function                                                         *
+ *************************************************************************/
+int main(int argc, const char **argv) {
+  const int width = WIDTH;
+  const int height = HEIGHT;
+
+  // host memory for image of width x height pixels
+  TYPE *input = load_data<TYPE>(width, height, 1, IMAGE);
+
+  // std::cout << "Calculating Hipacc Gaussian filter ..." << std::endl;
+
+  //************************************************************************//
+
+  // input and output image of width x height pixels
+  Image<TYPE> in(width, height, input);
+  Image<TYPE> out_dct(width, height);
+  Image<TYPE> out_fft(width, height);
+  Image<TYPE> out_mag_dct(width, height);
+  Image<TYPE> out_mag_fft(width, height);
+
+  // write fft of in to fftResult
+  float *fftResult = (float *)fft<TYPE, float>(in);
+  // create magnitude from fft
+  fftToMag<TYPE>(fftResult, out_mag_fft);
+  ifft<TYPE, float>(fftResult, out_fft);
+
+  float *dctResult = (float *)dct<TYPE, float>(in);
+  // create magnitude from fft
+  dctToMag<TYPE>(dctResult, out_mag_dct);
+  idct<TYPE, float>(dctResult, out_dct);
+
+  // get pointer to result data
+  TYPE *output_dct = out_dct.data();
+  TYPE *output_fft = out_fft.data();
+  TYPE *output_mag_dct = out_mag_dct.data();
+  TYPE *output_mag_fft = out_mag_fft.data();
+
+  save_data(width, height, 1, input, "input.jpg");
+  save_data(width, height, 1, output_dct, "output_dct.jpg");
+  save_data(width, height, 1, output_fft, "output_fft.jpg");
+  save_data(width, height, 1, output_mag_dct, "output_mag_dct.jpg");
+  save_data(width, height, 1, output_mag_fft, "output_mag_fft.jpg");
+
+
+  // manual transforms
+
+  TYPE2 test[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+  TYPE2 fft[4][(4 / 2 + 1) * 2];
+  TYPE2 dct[4][4];
+
+  fft_transform((TYPE2*)test, 4, 4, (TYPE2*)fft);
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < (4 / 2 + 1) * 2; x++) {
+      std::cout << fft[y][x] << "\t";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  dct_transform((TYPE2*)test, 4, 4, (TYPE2*)dct);
+
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      std::cout << dct[y][x] << "\t";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  // free memory
+  delete[] input;
+
+  return EXIT_SUCCESS;
+}
